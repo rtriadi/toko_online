d2496984ef0d99dd73629503771bd336b20167b3
Rahmat Triadi <rahmat_triadi@yahoo.com>
2020-06-29 22:00:15 +0800

    First Push

---
+++ app/Views/home/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+<div class="starter-template">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+</div>
+<?= $this->endSection() ?>
